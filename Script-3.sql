f397185c9edd583729cd7910970bfaa2adc1e9e4
SidorenkoPadla <116277079+SidorenkoPadla@users.noreply.github.com>
2023-01-18 20:25:06 +0300

    track fix

--- Script-3.sql
+++ Script-3.sql
@@ -1,29 +1,30 @@
 create table if not exists artist(
-artist_id SERIAL primary key,
-artist_name varchar(40) not null
+	artist_id SERIAL primary key,
+	artist_name varchar(40) not null
 );
 
 create table if not exists Genre(
-genre_id SERIAL primary key,
-genre_title varchar(40) not null
+	genre_id SERIAL primary key,
+	genre_title varchar(40) not null unique
 );
 
 create table if not exists Album(
-album_id SERIAL primary key references Track(album_id),
-album_title varchar(40) not null,
-year_of_release DATE not null
+	album_id SERIAL primary key,
+	album_title varchar(40) not null unique,
+	year_of_release DATE not null check(year_of_release > '1950-01-01')
 );
 
 create table if not exists Collection(
 	collection_id SERIAL primary key,
 	title_collection varchar(40) not null,
-	year_of_release DATE not null
+	year_of_release DATE not null check(year_of_release > '1950-01-01')
 );
 
 create table if not exists Track(
 	track_id SERIAL primary key,
-	duration TIME not null,
-	album_id SERIAL unique not null
+	duration TIME not null check(duration > '1:00'),
+	album_id int not null references Album(album_id),
+	track_title varchar(40) not null
 );
 
 create table if not exists Collection_track(
@@ -33,14 +34,14 @@ create table if not exists Collection_track(
 );
 
 create table if not exists Album_artist(
-artist_id INTEGER references artist(artist_id),
-album_id INTEGER references Album(album_id),
-constraint fk primary key (artist_id, album_id)
+	artist_id INTEGER references artist(artist_id),
+	album_id INTEGER references Album(album_id),
+	constraint fk primary key (artist_id, album_id)
 );
 
 create table if not exists Genre_artist(
-artist_id INTEGER references artist(artist_id),
-genre_id INTEGER references Genre(genre_id),
-constraint pk primary key (artist_id, genre_id)
+	artist_id INTEGER references artist(artist_id),
+	genre_id INTEGER references Genre(genre_id),
+	constraint pk primary key (artist_id, genre_id)
 );
 

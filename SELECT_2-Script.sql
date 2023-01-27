d601c707f37b46ab7babef208aceecf4939e50f5
SidorenkoPadla <116277079+SidorenkoPadla@users.noreply.github.com>
2023-01-27 20:45:24 +0300

    SELECT_2 Fix

--- SELECT_2-Script.sql
+++ SELECT_2-Script.sql
@@ -22,6 +22,34 @@ select distinct title_collection
 from collection c, collection_track ct, track t, album a, album_artist aa, artist a2
 where a2.artist_id = 1 and aa.album_id = a.album_id and a2.artist_id = aa.artist_id and t.album_id = a.album_id and t.track_id = ct.track_id and c.collection_id = ct.collection_id  
 
+select album_title 
+from artist a, genre_artist ga, album a2, album_artist aa 
+where a.artist_id = ga.artist_id and aa.album_id = a2.album_id and aa.artist_id = a.artist_id 
+group by album_title 
+having count(distinct ga.genre_id) > 1;
+
+select t.track_title
+from track t 
+left join collection_track ct on t.track_id = ct.track_id 
+where ct.collection_id is null;
+
+select artist_name
+from track t, artist a, album a2, album_artist aa 
+where duration = (select min(duration) from track t2) and a2.album_id = t.album_id and aa.album_id = a2.album_id and aa.artist_id = a.artist_id;
+
+select album_title
+from album a,track t
+where a.album_id = t.album_id
+group by album_title
+having count(track_id) = (select min(track_id) from track t2)
+
+
+
+
+
+
+
+
 
 
 
